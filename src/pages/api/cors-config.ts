2104d99253baf4a0282e74dd8ed8a97e8b16eff6
Alex Shyba <alexs@uniform.dev>
2023-02-22 19:46:22 -0800

    chore: cors config change

--- src/pages/api/cors-config.ts
+++ src/pages/api/cors-config.ts
@@ -1,7 +1,7 @@
 // Cross-Origin Resource Sharing to support Uniform platform Fake Commerce integration
 const corsConfig = {
   methods: ['GET', 'PUT', 'POST'],
-  origin: ['*'],
+  origin: [/.+-mesh-integration\.netlify\.app$/, 'https://fakecommerce.mesh.uniform.app', 'http://localhost:4020'],
   credentials: true,
 };
 
