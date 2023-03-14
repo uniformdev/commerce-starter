26c3c6ae43627e17a2c02894819484440d1588c5
Artem Miroshnyk <miroshnik1992@gmail.com>
2023-03-14 10:21:05 +0100

    feat: adding conditional banner to show on deployed by uniform environment in contextual editing

--- src/components-library/Announcement.tsx
+++ src/components-library/Announcement.tsx
@@ -1,37 +1,75 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Link from 'next/link';
+import { IN_CONTEXT_EDITOR_QUERY_STRING_PARAM } from '@uniformdev/canvas';
 import { BaseContainer } from '@/components-library/Container';
 
 const Announcement: React.FC = () => {
-  const [isOpened, setIsOpened] = useState(true);
+  const [isAnnouncementOpened, setIsAnnouncementOpened] = useState(true);
+  const [isDeployedBannerShown, setDeployedBannerShown] = useState(false);
 
-  const handleCloseButtonClick = useCallback(() => setIsOpened(false), [setIsOpened]);
+  const handleCloseButtonClick = useCallback(() => setIsAnnouncementOpened(false), [setIsAnnouncementOpened]);
 
-  if (!isOpened) return null;
+  const isOpenedByInContextEditor =
+    typeof window !== 'undefined' &&
+    new URLSearchParams(window.location.search).has(IN_CONTEXT_EDITOR_QUERY_STRING_PARAM);
+
+  // useEffect is necessary here to avoid hydration error caused by different
+  // render output based on typeof window
+  useEffect(() => {
+    if (isOpenedByInContextEditor) {
+      setDeployedBannerShown(Boolean(isOpenedByInContextEditor && process.env.NEXT_PUBLIC_SHOW_DEPLOYED_BANNER));
+    }
+  }, []);
+
+  if (!isAnnouncementOpened && !isDeployedBannerShown) return null;
+
   return (
-    <div className="bg-black animate-pop-in-announcement sticky top-0 py-3.5 z-50">
-      <BaseContainer>
-        <div className="relative">
-          <p className="text-white text-sm flex items-center md:justify-center w-full pr-16 md:pr-0">
-            <span className="truncate hidden sm:block mr-2">Continue building your project.</span>
-            <Link
-              className="text-teal-400 shrink-0 underline hover:no-underline cursor-pointer"
-              href="https://docs.uniform.app/getting-started/starters#javadrip"
-              target="_blank"
-            >
-              Learn about what do next with this Starter
-            </Link>
-          </p>
-          <button
-            aria-label="Dismiss announcement"
-            onClick={handleCloseButtonClick}
-            className="group cursor-pointer flex flex-row gap-2 items-center absolute right-0 top-1/2 -translate-y-1/2"
-          >
-            <p className="text-white group-hover:text-teal-400">Dismiss</p>
-          </button>
+    <>
+      {isDeployedBannerShown && (
+        <div className="bg-red-700 animate-pop-in-announcement sticky top-0 py-3.5 z-50">
+          <BaseContainer>
+            <div className="relative">
+              <p className="text-white text-sm flex items-center md:justify-center w-full pr-16 md:pr-0">
+                <span className="truncate hidden sm:block mr-2">This is a Uniform-deploy preview.</span>
+                <Link
+                  className="text-teal-400 shrink-0 underline hover:no-underline cursor-pointer"
+                  href="https://docs.uniform.app/getting-started/starters"
+                  target="_blank"
+                >
+                  Click here to learn how switch to your own!
+                </Link>
+              </p>
+            </div>
+          </BaseContainer>
         </div>
-      </BaseContainer>
-    </div>
+      )}
+      {isAnnouncementOpened && (
+        <div className="bg-black animate-pop-in-announcement sticky top-0 py-3.5 z-50">
+          <BaseContainer>
+            <div className="relative">
+              <p className="text-white text-sm flex items-center md:justify-center w-full pr-16 md:pr-0">
+                <span className="truncate hidden sm:block mr-2">Continue building your project.</span>
+                <Link
+                  className="text-teal-400 shrink-0 underline hover:no-underline cursor-pointer"
+                  href="https://docs.uniform.app/getting-started/starters#javadrip"
+                  target="_blank"
+                >
+                  Learn about what do next with this Starter
+                </Link>
+              </p>
+              <button
+                type="button"
+                aria-label="Dismiss announcement"
+                onClick={handleCloseButtonClick}
+                className="group cursor-pointer flex flex-row gap-2 items-center absolute right-0 top-1/2 -translate-y-1/2"
+              >
+                <p className="text-white group-hover:text-teal-400">Dismiss</p>
+              </button>
+            </div>
+          </BaseContainer>
+        </div>
+      )}
+    </>
   );
 };
 
