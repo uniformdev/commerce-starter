86a66934295803f1f9ed4829910c75d12271f8c5
Alex Shyba <alexs@uniform.dev>
2023-04-18 15:48:49 -0700

    feat: adding inline editing for the centered hero variant

--- src/components-canvas/Hero.tsx
+++ src/components-canvas/Hero.tsx
@@ -36,7 +36,6 @@ const HeroDefault: FC<HeroProps> = ({ buttonLink = '', backgroundImage }) => (
       <div className="bg-neutral-800 md:bg-orange-900 relative md:-bottom-11 ml-auto w-full md:max-w-[658px] p-12 md:pl-24 md:pr-7 md:py-20 z-10">
         <UniformText as="p" parameterId="title" className="font-bold text-3xl md:text-4xl lg:text-5xl text-white" />
         <UniformText as="p" parameterId="subtitle" className="mt-7 font-extrabold text-white" />
-
         {buttonLink && (
           <ButtonLink
             href={buttonLink}
@@ -55,10 +54,18 @@ const HeroCentered: FC<HeroProps> = ({ title, subtitle, buttonCopy = '', buttonL
       <Image className="absolute w-full h-full object-cover" src={backgroundImage} fill alt="hero-image" priority />
     )}
     <div className="relative flex items-center flex-col px-4 py-56 z-30">
-      <p className="text-white text-3xl md:text-5xl font-bold text-center max-w-[600px]">{title}</p>
-      {Boolean(subtitle) && <p className="mt-7 font-extrabold text-white">{subtitle}</p>}
+      <UniformText
+        as="p"
+        parameterId="title"
+        className="text-white text-3xl md:text-5xl font-bold text-center max-w-[600px]"
+      />
+      <UniformText as="p" parameterId="subtitle" className="mt-7 font-extrabold text-white" />
       {buttonCopy && buttonLink && (
-        <ButtonLink text={buttonCopy} href={buttonLink} className="mt-9 text-sm md:w-[329px] max-w-full" />
+        <ButtonLink
+          text={<UniformText parameterId="buttonCopy" />}
+          href={buttonLink}
+          className="mt-9 text-sm md:w-[329px] max-w-full"
+        />
       )}
     </div>
   </div>
