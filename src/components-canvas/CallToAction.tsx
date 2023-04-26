bd61679ffb04b71815f0013f57a4ec201e34250a
Adam Conn <adamc@uniform.dev>
2023-04-26 15:22:07 -0700

    feat: add OpenAI integration for CTA component

--- src/components-canvas/CallToAction.tsx
+++ src/components-canvas/CallToAction.tsx
@@ -6,32 +6,37 @@ import ButtonLink from '@/components-library/ButtonLink';
 type CallToActionProps = ComponentProps<{
   title: string;
   text: string;
+  textAi?: string;
   buttonCopy?: string;
   buttonLink?: string;
 }>;
 
 type ExtendedCallToActionProps = Omit<CallToActionProps, 'buttonLink'> & {
   buttonLink?:
-    | {
-        path: string;
-        nodeId: string;
-        projectMapId: string;
-      }
-    | string;
+  | {
+    path: string;
+    nodeId: string;
+    projectMapId: string;
+  }
+  | string;
 };
 
-const CallToAction: FC<CallToActionProps> = ({ buttonLink }) => (
-  <Container paddingTop={PaddingSize.Large} paddingBottom={PaddingSize.Large}>
-    <div className="md:w-9/12 m-auto">
-      <UniformText parameterId="title" as="p" className="md:text-center font-bold text-4xl" />
-      <UniformText parameterId="text" as="p" isMultiline className="md:text-center mt-6" />
+const CallToAction: FC<CallToActionProps> = ({ buttonLink, textAi }) => {
+  const showGeneratedText = textAi && textAi.trim().length > 0
+  const parameterId = showGeneratedText ? 'textAi' : 'text'
+  return (
+    <Container paddingTop={PaddingSize.Large} paddingBottom={PaddingSize.Large}>
+      <div className="md:w-9/12 m-auto">
+        <UniformText parameterId="title" as="p" className="md:text-center font-bold text-4xl" />
+        <UniformText parameterId={parameterId} as="p" isMultiline className="md:text-center mt-6" />
 
-      {buttonLink && (
-        <ButtonLink href={buttonLink} text={<UniformText parameterId="buttonCopy" />} className="mt-8 px-4" />
-      )}
-    </div>
-  </Container>
-);
+        {buttonLink && (
+          <ButtonLink href={buttonLink} text={<UniformText parameterId="buttonCopy" />} className="mt-8 px-4" />
+        )}
+      </div>
+    </Container>
+  )
+};
 
 const transformData = (BaseComponent: FC<CallToActionProps>): FC<CallToActionProps> =>
   function wrapper({ buttonLink, ...props }: ExtendedCallToActionProps) {
